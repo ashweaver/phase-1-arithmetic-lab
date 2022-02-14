30d20e754179f4fc36e703cf6ec7b86e0bd05a95
ashweaver <97565194+ashweaver@users.noreply.github.com>
2022-02-14 08:54:11 -0600

    commit tree 4f56ad76d7efe032fb158ca628ca7a744ed83506

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const num1 = 62
+const num2 = 1;
+const num3 = 19;
+const num4 = 5;
 
-// Write your code here
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 10);
+const mod = num3 % num4;
+const max = Math.max(1, 8,20, 16);
